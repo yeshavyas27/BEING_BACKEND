054e0c16f93aa0214f439c83d7f7d60f994f0041
HetviPatel-2002 <ptlhetvi1126@gmail.com>
2024-06-05 23:11:48 +0530

    made login file

--- resources/user/login.py
+++ resources/user/login.py
@@ -1,7 +1,18 @@
-
-from flask_restful import Resource
-
+from flask import Flask, request
+from flask_restful import Resource, Api
+import pymongo
+from global_utilities import users
 
 class Login(Resource):
     def post(self):
-        pass
+        data = request.get_json()
+        email_id = data.get('email_id')
+        password = data.get('password')
+
+        myquery = {"email_id": email_id}
+        user = users.find_one(myquery)
+
+        if user and user.get('password') == password:
+            return {"Message": "Login Successful!"}
+        else:
+            return {"Message": "Invalid Email or Password!"}, 401  # 401 for unauthorized.
