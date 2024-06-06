54b31c3bf01e7dbf2bb60a8ea3d913af2226d74b
HetviPatel-2002 <ptlhetvi1126@gmail.com>
2024-06-07 01:25:50 +0530

    added the get method

--- resources/exercise.py
+++ resources/exercise.py
@@ -2,12 +2,10 @@ import json
 import traceback
 from http import HTTPStatus
 
-#
-from bson import json_util
 from flask import request, make_response
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_restful import Resource
-#
+
 from database.exercise import ExerciseRepository
 from global_utilities import logging_utilities
 from utilities.exceptions import Exceptions
@@ -23,7 +21,7 @@ class Exercise(Resource):
         try:
             data = validate_request(request)
         except Exceptions as exception:
-            self.logger.error(f"Exception raised in Post Exercise API. Traceback:\n{traceback.print_exc()}")
+            self.logger.error(f"Exception raised in Post Exercise API. Traceback:\n{traceback.format_exc()}")
             response_payload = {
                 "status": "FAILURE",
                 "message": exception.message
@@ -35,16 +33,16 @@ class Exercise(Resource):
 
         name = data.get("name")
         image_url = data.get("image_url")
-        instruction = data.get("instruction",None)
-        tags = data.get("tags",None)
-        user_id= get_jwt_identity()
+        instruction = data.get("instruction", None)
+        tags = data.get("tags", None)
+        user_id = get_jwt_identity()
 
         try:
             response_payload = ExerciseRepository().insert(name=name, image_url=image_url, created_by=user_id,
                                                            instruction=instruction, tags=tags)
         except Exceptions as exception:
             self.logger.error(
-                f"Exception raised in inserting new exercise in database . Traceback:\n{traceback.print_exc()}")
+                f"Exception raised in inserting new exercise in database. Traceback:\n{traceback.format_exc()}")
             response_payload = {
                 "status": "FAILURE",
                 "message": exception.message
@@ -52,13 +50,38 @@ class Exercise(Resource):
             response = make_response(json.dumps(response_payload))
             response.status_code = exception.status_code
             response.mimetype = 'application/json'
-
             return response
 
-        response = make_response(json.dumps(response_payload))
+        response = make_response(json.dumps(response_payload), HTTPStatus.CREATED)
         response.mimetype = 'application/json'
-
         return response
 
     def get(self, exercise_name):
-        return {"message": "exercise"}
+        try:
+            exercise = ExerciseRepository().find_by_name(exercise_name)
+            if not exercise:
+                response_payload = {
+                    "status": "FAILURE",
+                    "message": "Exercise not found"
+                }
+                response = make_response(json.dumps(response_payload), HTTPStatus.NOT_FOUND)
+                response.mimetype = 'application/json'
+                return response
+
+            response_payload = {
+                "status": "SUCCESS",
+                "exercise": exercise
+            }
+            response = make_response(json.dumps(response_payload), HTTPStatus.OK)
+            response.mimetype = 'application/json'
+            return response
+
+        except Exceptions as exception:
+            self.logger.error(f"Exception raised in Get Exercise API. Traceback:\n{traceback.format_exc()}")
+            response_payload = {
+                "status": "FAILURE",
+                "message": exception.message
+            }
+            response = make_response(json.dumps(response_payload), exception.status_code)
+            response.mimetype = 'application/json'
+            return response
